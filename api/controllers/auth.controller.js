e5bdae5560b0156992bc16722aab01ba9085f3d9
Gazal Kagdi <gazalkagdi111@gmail.com>
2024-04-14 18:43:13 +0530

    middleware/function to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import bcryptjs from 'bcryptjs';
 import User from "../models/user.model.js";
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || username == '' || email == '' || password == '') {
-        return res.status(400).json("Required Fields Missing");
+        next(errorHandler(400, 'All fields are required'))
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -20,7 +21,7 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.json({ message: "Signup Successful" });
     } catch (error) {
-        res.json(error);
+        next(error);
     }
 
 }

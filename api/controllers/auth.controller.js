f8c7c847b59042540243e1675efc653bd0dcbbe4
Gazal Kagdi <gazalkagdi111@gmail.com>
2024-04-15 17:46:33 +0530

    created sign-in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import User from "../models/user.model.js";
 import { errorHandler } from '../utils/error.js';
 
@@ -25,4 +26,31 @@ export const signup = async (req, res, next) => {
         next(error);
     }
 
+}
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password || email == '' || password == '') {
+        next(errorHandler(400, 'Missing required fields'));
+        return;
+    }
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(400, 'User not found'));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(400, 'Invalid Password'));
+        }
+
+        const { password: pass, ...rest } = validUser._doc;
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+        res.status(200).cookie('access_token', token, { httpOnly: true }).json(rest);
+
+    } catch (error) {
+        next(error);
+    }
 }

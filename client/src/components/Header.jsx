7994a5c938f8f90bf6eaa6339d33d40575b4ef43
Gazal Kagdi <gazalkagdi111@gmail.com>
2024-04-21 23:30:29 +0530

    adding user data to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Navbar, TextInput, Button } from 'flowbite-react';
+import { Navbar, TextInput, Button, Dropdown, Avatar } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai'
 import { FaMoon } from 'react-icons/fa'
+import { useSelector } from 'react-redux'
 
 export default function Header() {
     const path = useLocation().pathname;
+    const { currentUser } = useSelector(state => state.user);
 
     return (
-        <Navbar className='border-b-2'>
+        <Navbar className='border-b-2 fluid'>
             <Link className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'> <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Gazal's</span> Blog</Link>
             <form >
                 <TextInput
@@ -25,9 +27,38 @@ export default function Header() {
                 <Button className='w-12 hidden sm:inline' color='gray' pill>
                     <FaMoon />
                 </Button>
-                <Button href='/sign-up' gradientDuoTone='purpleToBlue' outline >
-                    Sign In
-                </Button>
+                {currentUser ?
+                    (
+                        <Dropdown
+                            arrowIcon={false}
+                            inline
+                            label={
+                                <Avatar
+                                    alt='user'
+                                    img={currentUser.profilePicture}
+                                    rounded
+                                />
+                            }
+                        >
+                            <Dropdown.Header>
+                                <span className='block text-sm'>@{currentUser.username}</span>
+                                <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+                            </Dropdown.Header>
+                            <Link to='/dashboard?tab=profile'>
+                                <Dropdown.Item>Profile</Dropdown.Item>
+                            </Link>
+                            <Dropdown.Divider />
+                            <Dropdown.Item>Sign out</Dropdown.Item>
+                        </Dropdown>
+                    ) :
+                    (
+                        <Button href='/sign-up' gradientDuoTone='purpleToBlue' outline >
+                            Sign In
+                        </Button>
+
+                    )
+                }
+
                 <Navbar.Toggle />
             </div>
             <Navbar.Collapse>

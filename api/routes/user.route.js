8b147ce98163ee4671910172b4f6e856a494e8ff
Gazal Kagdi <gazalkagdi111@gmail.com>
2024-04-30 15:44:42 +0530

    created user delete api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,11 @@
-import { test, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, test, updateUser } from "../controllers/user.controller.js";
 import express from 'express';
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get('/test', test);
-
 router.put('/update/:userId', verifyToken, updateUser);
+router.delete('/delete/:userId', deleteUser);
 
 export default router;

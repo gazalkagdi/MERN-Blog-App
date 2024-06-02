f00f7bf30de6145ef48ea1ab63e5e08564a2e938
Gazal Kagdi <gazalkagdi111@gmail.com>
2024-06-02 15:18:35 +0530

    added recent articles section

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,12 +3,14 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
 import CommentSection from "../components/CommentSection";
+import PostCard from "../components/PostCard";
 
 export default function PostPage() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [post, setPost] = useState(null);
+  const [recentPost, setRecentPost] = useState(null);
 
   useEffect(() => {
     const getPost = async () => {
@@ -35,6 +37,23 @@ export default function PostPage() {
     getPost();
   }, [postSlug]);
 
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?limit=3`, {
+          method: "GET",
+        });
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPost(data.posts);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getPosts();
+  }, []);
+
   if (loading)
     return (
       <div className="flex justify-center items-center min-h-screen">
@@ -43,37 +62,46 @@ export default function PostPage() {
     );
 
   return (
-    <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
-      <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
-        {post && post.title}
-      </h1>
-      <Link
-        className="self-center mt-5"
-        to={`/search?category=${post && post.category}`}
-      >
-        <Button color="gray" pill size="xs">
-          {post && post.category}
-        </Button>
-      </Link>
-      <img
-        src={post && post.image}
-        alt={post && post.title}
-        className="mt-10 p-3 max-h-[600px] w-full object-cover"
-      />
-      <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
-        <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
-        <span className="italic">
-          {post && (post.content.length / 1000).toFixed()} mins read
-        </span>
+    <>
+      <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
+        <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
+          {post && post.title}
+        </h1>
+        <Link
+          className="self-center mt-5"
+          to={`/search?category=${post && post.category}`}
+        >
+          <Button color="gray" pill size="xs">
+            {post && post.category}
+          </Button>
+        </Link>
+        <img
+          src={post && post.image}
+          alt={post && post.title}
+          className="mt-10 p-3 max-h-[600px] w-full object-cover"
+        />
+        <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
+          <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
+          <span className="italic">
+            {post && (post.content.length / 1000).toFixed()} mins read
+          </span>
+        </div>
+        <div
+          className="p-3 max-w-2xl mx-auto w-full post-content"
+          dangerouslySetInnerHTML={{ __html: post && post.content }}
+        ></div>
+        <div className="max-w-4xl mx-auto w-full">
+          <CallToAction />
+        </div>
+        <CommentSection postId={post && post._id} />
+      </main>
+      <div className="flex flex-col justify-center items-center mb-5">
+        <h1 className="text-xl mt-5">Recent articles</h1>
+        <div className="flex flex-wrap gap-5 mt-5 justify-center">
+          {recentPost &&
+            recentPost.map((post) => <PostCard key={post._id} post={post} />)}
+        </div>
       </div>
-      <div
-        className="p-3 max-w-2xl mx-auto w-full post-content"
-        dangerouslySetInnerHTML={{ __html: post && post.content }}
-      ></div>
-      <div className="max-w-4xl mx-auto w-full">
-        <CallToAction />
-      </div>
-      <CommentSection postId={post && post._id} />
-    </main>
+    </>
   );
 }
